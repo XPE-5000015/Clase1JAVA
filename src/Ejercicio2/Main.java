b3021d8d54bfa9c1c44638bb0ec00b8557d76ec4
XPE-5000015 <juan.palomo@xpertal.com>
2022-01-19 14:49:42 -0600

    Primer commit del Ejercicio2 de JAVA.

---
+++ src/Ejercicio2/Main.java
@@ -0,0 +1,14 @@
+package Ejercicio2;
+
+public class Main {
+    public static void main(String[] args) {
+        String nombre1;
+        int edad;
+        double sueldo;
+        String apellido;
+        String direccion;
+        boolean licenciaDeConducir;
+        double estaturaDeLaPersona;
+        int cantidadDeHijos;
+    }
+}
